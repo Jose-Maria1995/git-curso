8c2f6bc341433d3473f4dc53559fb4725a16ce41
Jose <josemarianarganes@gmail.com>
2020-05-26 16:26:33 +0200

    He agregado un .gitignore

--- PilaEnteros.java
+++ PilaEnteros.java
@@ -1,57 +1,1 @@
-package pilasnodos;
-
-public class PilaEnteros {
-	
-	private Nodo cima;
-	
-	public PilaEnteros() {
-		
-		this.cima = null;
-	}
-	
-	public boolean push(Integer i) {
-		Nodo num=new Nodo(i);
-		num.siguiente =this.cima;
-		this.cima=num;
-		return true;
-	}
-	
-	public Integer pop() {
-		Integer salida = this.cima.getValor();
-		this.cima=this.cima.getSiguiente();
-		return salida;
-	}
-	
-	public Integer top() {
-		
-		if(this.cima!=null) {
-			Integer consulta=this.cima.getValor();
-			return consulta;
-			}
-		
-		else {
-			Integer consulta = null;
-			return consulta;
-		}
-		
-	}
-	
-	public boolean isEmpty() {
-		boolean verificacion;
-		if(this.cima==null) {
-			verificacion = true;
-		}
-		else {
-			verificacion =false;
-		}
-		return verificacion;
-	}
-	
-	public boolean isFull() {
-		boolean verificacion = false;
-		
-		return verificacion;
-		
-	}
-
-}
+package pilasnodos;public class PilaEnteros {	private Nodo cima;	private int prueba;	public PilaEnteros() {				this.cima = null;	}		public boolean push(Integer i) {		Nodo num=new Nodo(i);		num.siguiente =this.cima;		this.cima=num;		return true;	}		public Integer pop() {		Integer salida = this.cima.getValor();		this.cima=this.cima.getSiguiente();		return salida;	}		public Integer top() {				if(this.cima!=null) {			Integer consulta=this.cima.getValor();			return consulta;			}				else {			Integer consulta = null;			return consulta;		}			}		public boolean isEmpty() {		boolean verificacion;		if(this.cima==null) {			verificacion = true;		}		else {			verificacion =false;		}		return verificacion;	}		public boolean isFull() {		boolean verificacion = false;				return verificacion;			}}
